1555b6cd1a3d3fccb8a3c82a7965e435ab9b5ddf
arjit <arjit.nitd@outlook.com>
2020-09-20 15:28:36 +0530

    Init

---
+++ src/main/java/legend/spring/springdemoApp/SpringdemoAppApplication.java
@@ -0,0 +1,13 @@
+package legend.spring.springdemoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/legend/spring/springdemoApp/SpringdemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package legend.spring.springdemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
